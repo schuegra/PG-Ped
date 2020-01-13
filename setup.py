4b018f6f08bc42c93fb4af9085920f8e17dcc3a1
schuegra <philipp.schuegraf@hm.edu>
2020-01-13 18:08:08 +0100

    .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PGPed',
+    version='0.1',
+    packages=['pg_ped'],
+    url='',
+    license='',
+    author='Philipp Schuegraf',
+    author_email='',
+    description=''
+)
